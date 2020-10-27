92f21896e530edbc37b41987c8ebecd59417385b
Matthias Ender <matthias.ender@sas.com>
2020-10-27 13:19:38 -0400

    create vm and pg helper objects conditionally additional terraform fmt applied

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -1,19 +1,20 @@
 resource "random_id" "suffix" {
+  count       = var.create_postgres ? 1 : 0
   byte_length = 4
 }
 
 resource "google_sql_database_instance" "utility-database" {
-  name             = "${var.name}-${random_id.suffix.hex}"
+  name = "${var.name}-${random_id.suffix[0].hex}"
 
-  count            = var.create_postgres ? 1 : 0
+  count = var.create_postgres ? 1 : 0
 
   database_version = "POSTGRES_${var.server_version}"
-  region           = regex("^[a-z0-9]*-[a-z0-9]*",var.location)
+  region           = regex("^[a-z0-9]*-[a-z0-9]*", var.location)
 
-  depends_on       = [google_service_networking_connection.private_vpc_connection]
+  depends_on = [google_service_networking_connection.private_vpc_connection]
 
   settings {
-    tier        = var.machine_type  # 30Gb needed to get 600 max_connections default: https://cloud.google.com/sql/docs/postgres/quotas
+    tier        = var.machine_type # 30Gb needed to get 600 max_connections default: https://cloud.google.com/sql/docs/postgres/quotas
     disk_size   = var.disk_size_gb
     user_labels = var.labels
 
@@ -21,7 +22,7 @@ resource "google_sql_database_instance" "utility-database" {
       private_network = var.network
       require_ssl     = var.ssl_enforcement_enabled
 
-      ipv4_enabled    = length(var.public_access_cidrs) > 0 ? true : false
+      ipv4_enabled = length(var.public_access_cidrs) > 0 ? true : false
       dynamic "authorized_networks" {
         for_each = var.public_access_cidrs
         iterator = cidr
@@ -36,12 +37,12 @@ resource "google_sql_database_instance" "utility-database" {
 # There is currently no way to set the admin password for the postgres database instance 
 # so we create a separate database and user 
 # Also of note:: https://github.com/terraform-providers/terraform-provider-google/issues/3820
-  
+
 resource "google_sql_database" "database" {
-  name        = var.administrator_login
-  instance    = google_sql_database_instance.utility-database[0].name
-  count       = var.create_postgres ? 1 : 0
-  depends_on  = [google_sql_user.pgadmin]
+  name       = var.administrator_login
+  instance   = google_sql_database_instance.utility-database[0].name
+  count      = var.create_postgres ? 1 : 0
+  depends_on = [google_sql_user.pgadmin]
 }
 
 resource "google_sql_user" "pgadmin" {
@@ -56,8 +57,8 @@ resource "google_sql_user" "pgadmin" {
 # https://cloud.google.com/sql/docs/postgres/private-ip
 
 resource "google_compute_global_address" "private_ip_address" {
-  name     = "${var.name}-private-ip-address"
-  count    = var.create_postgres ? 1 : 0
+  name  = "${var.name}-private-ip-address"
+  count = var.create_postgres ? 1 : 0
 
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
@@ -67,7 +68,7 @@ resource "google_compute_global_address" "private_ip_address" {
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
-  count    = var.create_postgres ? 1 : 0
+  count = var.create_postgres ? 1 : 0
 
   network                 = var.network
   service                 = "servicenetworking.googleapis.com"
--- modules/postgresql/outputs.tf
+++ modules/postgresql/outputs.tf
@@ -1,19 +1,19 @@
 
 output "postgres_server_private_ip" {
   description = "Private IP of the PostgreSQL server. Use this value to set DATABASE_HOST in your Viya deployment."
-  value       = ( var.create_postgres ? 
-                   ( length(google_sql_database_instance.utility-database) > 0 
-                    ? google_sql_database_instance.utility-database[0].private_ip_address 
-                    : null )
-                : null )
+  value = (var.create_postgres ?
+    (length(google_sql_database_instance.utility-database) > 0
+      ? google_sql_database_instance.utility-database[0].private_ip_address
+    : null)
+  : null)
 }
 
 output "postgres_server_public_ip" {
   description = "Public IP of the PostgreSQL server. Use this value for client access."
-  value       = ( var.create_postgres ? 
-                   ( length(google_sql_database_instance.utility-database) > 0 
-                    ? google_sql_database_instance.utility-database[0].public_ip_address 
-                    : null )
-                : null )
+  value = (var.create_postgres ?
+    (length(google_sql_database_instance.utility-database) > 0
+      ? google_sql_database_instance.utility-database[0].public_ip_address
+    : null)
+  : null)
 }
 
