87522eddd83fd2086a683e95fd4021ef3267fb2a
Matthias Ender <matthias.ender@sas.com>
2021-04-28 11:14:38 -0400

    (IAC-241) deal with partially destroyed network

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,14 +1,14 @@
 output "network_name" {
-  value = length(var.vpc_name) == 0 ? google_compute_network.vpc.0.name : var.vpc_name
+  value = length(var.vpc_name) == 0 ? element(coalescelist(google_compute_network.vpc.*.name,[" "]),0) : var.vpc_name
 }
 
 output "network_self_link" {
-  value = length(var.vpc_name) == 0 ? google_compute_network.vpc.0.self_link : data.google_compute_network.vpc.0.self_link
+  value = length(var.vpc_name) == 0 ? element(coalescelist(google_compute_network.vpc.*.self_link,[" "]),0) : data.google_compute_network.vpc.0.self_link
 }
 
 output subnets {
   value = {
-    gke : var.create_subnets ? google_compute_subnetwork.gke_subnet.0 : data.google_compute_subnetwork.gke_subnet.0
-    misc: var.create_subnets ? google_compute_subnetwork.misc_subnet.0 : data.google_compute_subnetwork.misc_subnet.0
+    gke : var.create_subnets ? element(coalescelist(google_compute_subnetwork.gke_subnet,[" "]),0) : data.google_compute_subnetwork.gke_subnet.0
+    misc: var.create_subnets ? element(coalescelist(google_compute_subnetwork.misc_subnet,[" "]),0) : data.google_compute_subnetwork.misc_subnet.0
   }
 }
