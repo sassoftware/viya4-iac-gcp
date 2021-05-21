311a71c9275830305d7eb190dc1ed42c5a6a76f0
Thomas S. Pangborn <thomas.pangborn@sas.com>
2021-05-21 09:07:03 -0400

    Fixed subnet output issues

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -8,7 +8,7 @@ output "network_self_link" {
 
 output subnets {
   value = {
-    gke  : var.create_subnets ? google_compute_subnetwork.gke_subnet.0  : data.google_compute_subnetwork.gke_subnet.0
-    misc : var.create_subnets ? google_compute_subnetwork.misc_subnet.0 : data.google_compute_subnetwork.misc_subnet.0
+    gke  : var.create_subnets ? try(google_compute_subnetwork.gke_subnet.0, null)  : try(data.google_compute_subnetwork.gke_subnet.0, null)
+    misc : var.create_subnets ? try(google_compute_subnetwork.misc_subnet.0, null) : try(data.google_compute_subnetwork.misc_subnet.0, null)
   }
 }
