9f116fb112ec044758cc9335027b249a3967cf8f
jarpat <jay.patel@sas.com>
2022-08-24 14:53:01 -0400

    update private-cluster module & remove deprecated kubernetes provider arg

--- versions.tf
+++ versions.tf
@@ -12,7 +12,7 @@ terraform {
     }
     kubernetes  = {
       source  = "hashicorp/kubernetes"
-      version = "2.13.0"
+      version = "2.13.0" # Constrained by Google
     }
     local       = {
       source  = "hashicorp/local"
