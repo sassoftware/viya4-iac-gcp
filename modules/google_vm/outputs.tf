822ee160963d160cc97cc6240fc2fbd691f6a651
jarpat <jay.patel@sas.com>
2023-03-09 10:58:00 -0500

    add copyright to all .tf files

--- modules/google_vm/outputs.tf
+++ modules/google_vm/outputs.tf
@@ -1,3 +1,6 @@
+# Copyright © 2021-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 output "private_ip" {
   value = google_compute_instance.google_vm.network_interface.0.network_ip
 }
