a177477fa8874f3917e188e288da286a2111d407
Lohit Dave <lohit.dave@sas.com>
2026-07-15 17:47:36 +0530

    added nfsv4.1 protocol support

--- modules/google_netapp/main.tf
+++ modules/google_netapp/main.tf
@@ -69,8 +69,7 @@ resource "google_netapp_volume" "netapp-nfs-volume" {
       allowed_clients = var.allowed_clients
       has_root_access = true
       nfsv3           = contains(var.protocols, "NFSV3") ? true : false
-      nfsv4           = contains(var.protocols, "NFSV4") ? true : false
-      nfsv41          = contains(var.protocols, "NFSV4_1") ? true : false
+      nfsv4           = contains(var.protocols, "NFSV4") || contains(var.protocols, "NFSV4_1") ? true : false
     }
   }
 
