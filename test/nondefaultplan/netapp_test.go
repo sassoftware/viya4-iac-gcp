69a2d8265018da7ef56fdca290971139eba651d8
abhishek kumar <abhishek.kumar@sas.com>
2026-07-30 10:39:37 +0530

    chore: fixed unittest for netapp

--- test/nondefaultplan/netapp_test.go
+++ test/nondefaultplan/netapp_test.go
@@ -23,7 +23,7 @@ func TestPlanNetApp(t *testing.T) {
 	variables["default_nodepool_locations"] = "us-east1-b,us-east1-c"
 	variables["nodepools_locations"] = "us-east1-b,us-east1-c"
 	variables["regional"] = true
-	variables["netapp_protocols"] = []string{"NFSV3"}
+	variables["netapp_protocols"] = []string{"NFSV4_1"}
 
 	tests := map[string]helpers.TestCase{
 		"poolExists": {
